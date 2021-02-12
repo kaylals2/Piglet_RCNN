4f918a8099549c2389ccdf67b8a092256253a562
kaylals2 <61709901+kaylals2@users.noreply.github.com>
2021-02-12 12:08:14 -0800

    Update resample_nii.py

--- resample_nii.py
+++ resample_nii.py
@@ -2,6 +2,7 @@ import os
 import nibabel as nib
 from nibabel.processing import resample_from_to
 
+#Subject masks are mapped to voxel space of structural images (256, 288, 288), without interpolation
 
 BASE_FOLDER = 'C:\\Users\\Kayla\\Desktop\\scripts\\manual_extraction_files'
 INPUT_NAME = 'manual_brain_mask-label.nii'
@@ -9,7 +10,7 @@ TEMPLATE_NAME = 'oT1.nii'
 APPEND_OUTPUT = '-resampled'
 
 
-# Subject masks are mapped to voxel space of structural images (256, 288, 288), without interpolation
+# Function to apply affine of structural image to manual masks
 def resample(input_path, template_path):
     input_ = nib.load(input_path)
     template = nib.load(template_path)
@@ -17,8 +18,9 @@ def resample(input_path, template_path):
     output = resample_from_to(input_, template, order=0)
     print(template.shape,input_.shape,output.shape)
     nib.save(output, os.path.splitext(input_path)[0] + APPEND_OUTPUT + '.nii')
-
-for folder in os.listdir(BASE_FOLDER): # list all files/dirs in folder
+    
+# Iterates through folders to apply function to each subject 
+for folder in os.listdir(BASE_FOLDER): # list all files/directories in base folder
     folder_path = os.path.join(BASE_FOLDER, folder) # join arguments with os specific seperator.
     if os.path.isdir(folder_path): # only select directories.
         print(folder)
